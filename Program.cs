13e3013487f6375b96ea801826c53af713f54a86
Mateus Morais <mateus87morais@gmail.com>
2022-08-30 17:49:20 -0700

    Initial

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿var watch = new System.Diagnostics.Stopwatch();
+
+watch.Start();
+var array = new int[] { 99, 65, 33, 1, 2, 54, 11, 15, 91, 3, 15 };
+Console.WriteLine("Original Array:");
+Console.WriteLine("[{0}]", string.Join(", ", array));
+//Depending of the number of elements and max value in your array this is going to take very long, but it is going to be fun :)
+var orderedArray = BogoSortClass.BogoSort(array);
+Console.WriteLine("Ordered Array:");
+Console.WriteLine("[{0}]", string.Join(", ", array));
+
+watch.Stop();
+
+Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+
